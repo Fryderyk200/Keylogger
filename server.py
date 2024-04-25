70db29e1f1dae2fba8ef97244858d9d8195f0ddf
Fryderyk200 <146193454+Fryderyk200@users.noreply.github.com>
2024-04-25 22:26:26 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import socket
 import ssl
 from cryptography.fernet import Fernet
+#Server IP address and port
 HOST = '0.0.0.0'
 PORT = 9090
+#File name where  the decrypted data will be stored
 FILE_NAME = 'received.txt'
 
 def load_key():
+    # attempt to load encrypted key from file
         try:
             with open("current_key.key", "rb") as key_file:
                 key = key_file.read()
                 return key
         except FileNotFoundError:
+            # exeption if file not found print error message
             print("Encryption key file not fount")
             return None
 
-#decrypt message
+#decrypt message using fernet
 def decrypt_message(encrypted_message):
     key = load_key()
     if key is None:
@@ -25,11 +29,12 @@ def decrypt_message(encrypted_message):
     return decrypted_message.decode()
 
 def handle_client_connection(conn):
+    # initial response to the client
     response_message = 'continue'
     conn.sendall(response_message.encode('utf-8'))
     print("Sent 'continue' command to client.")
 
-    #decrypt data
+    #recive decrypt data
     encrypted_data = bytearray()
     while True:
         data = conn.recv(1024)
@@ -45,7 +50,7 @@ def handle_client_connection(conn):
     except Exception as e:
         print(f"Failed to decrypt data: {e}")
 
-
+# Creates an ssl connection for secure communications
 def create_ssl_context():
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
@@ -61,16 +66,21 @@ def main():
         s.listen(5)
 
         while True:
+            # accepts connection from clients
             conn, addr = s.accept()
             print(f"Accepted new connection from: {addr}")
+            # wraps the connection with SSL
             with context.wrap_socket(conn, server_side=True) as secure_conn:
+                # log initial message from client
                 connected = secure_conn.recv(1024)
                 print(connected)
+                # receive a new encryption key from client
                 receive_key = secure_conn.recv(1024)
                 with open("current_key.key", "wb") as key_file:
                     key_file.write(receive_key)
                 print("Received and saved new encryption key.")
 
+                # handle further client communications
                 handle_client_connection(secure_conn)
 
 
